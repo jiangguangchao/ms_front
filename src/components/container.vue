2493378bf5723daeea7e4e93f861aac1fc73b0b9
jgc <jiangguangchao157@163.com>
2021-03-19 18:40:47 +0800

    菜单列表增加首页

--- src/components/container.vue
+++ src/components/container.vue
@@ -5,22 +5,26 @@
     <el-container>
       <el-aside width="200px">
         <el-menu default-active="2">
-          <el-submenu index="1">
+          <el-menu-item @click="home" index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>订单</span>
             </template>
-            <el-menu-item index="1-1" @click="orderMag">订单管理</el-menu-item>
-            <el-menu-item index="1-2">进货管理</el-menu-item>
+            <el-menu-item index="2-1" @click="orderMag">订单管理</el-menu-item>
+            <el-menu-item index="2-2">进货管理</el-menu-item>
           </el-submenu>
 
-          <el-submenu index="2">
+          <el-submenu index="3">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>客户</span>
             </template>
-            <el-menu-item index="2-1">订单管理</el-menu-item>
-            <el-menu-item index="2-2">进货管理</el-menu-item>
+            <el-menu-item index="3-1">订单管理</el-menu-item>
+            <el-menu-item index="3-2">进货管理</el-menu-item>
           </el-submenu>
 
         </el-menu>
@@ -40,6 +44,9 @@ export default {
   methods: {
     orderMag() {
       this.$router.push('/container/forder')
+    },
+    home() {
+      this.$router.push('/container/home')
     }
   }
 }
